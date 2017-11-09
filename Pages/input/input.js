fc474e5ec9398ba592bc1cf31276c1884519c59f
Frank Steiner <FrankThomas.Steiner@infineon.com>
2017-11-09 14:47:52 +0100

    update

---
+++ Pages/input/input.js
@@ -0,0 +1,27 @@
+checkInput = () =>{
+    let inForm = document.getElementById("inputForm");
+
+    if(inForm.checkValidity()){
+     dataFile ={};
+     dataFile["prename"]= document.getElementById("prenameIn").value;
+     dataFile["lastname"]= document.getElementById("lastnameIn").value;
+     dataFile["username"]= document.getElementById("usernameIn").value;
+     dataFile["email"]= document.getElementById("emailIn").value;
+     dataFile["password"] = document.getElementById("passwordIn").value;
+
+
+    }
+}
+
+var passsword = document.getElementById("passwordIn");
+
+passsword.addEventListener("input", function (event) {
+  if (passsword.validity.patternMismatch) {
+    passsword.setCustomValidity("You need to add at least one of these characters: ?!=#+-_%$§");
+  } else {
+    passsword.setCustomValidity("");
+  }
+});
+
+
+const x = open("C:/Users/SteineFr/PycharmProjects/webtechnickenSolo/Ressources/data/Userdata.json", "r")
