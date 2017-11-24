e4c52466c99bc43910ea591f3bfb4e7f3d0f2ae8
Frank Steiner <FrankThomas.Steiner@infineon.com>
2017-11-24 16:04:16 +0100

    grid try

--- Pages/input/input.js
+++ Pages/input/input.js
@@ -1,13 +1,13 @@
-checkInput = () =>{
+checkInput = () => {
     let inForm = document.getElementById("inputForm");
 
-    if(inForm.checkValidity()){
-     dataFile ={};
-     dataFile["prename"]= document.getElementById("prenameIn").value;
-     dataFile["lastname"]= document.getElementById("lastnameIn").value;
-     dataFile["username"]= document.getElementById("usernameIn").value;
-     dataFile["email"]= document.getElementById("emailIn").value;
-     dataFile["password"] = document.getElementById("passwordIn").value;
+    if (inForm.checkValidity()) {
+        dataFile = {};
+        dataFile["prename"] = document.getElementById("prenameIn").value;
+        dataFile["lastname"] = document.getElementById("lastnameIn").value;
+        dataFile["username"] = document.getElementById("usernameIn").value;
+        dataFile["email"] = document.getElementById("emailIn").value;
+        dataFile["password"] = document.getElementById("passwordIn").value;
 
 
     }
@@ -16,10 +16,10 @@ checkInput = () =>{
 var passsword = document.getElementById("passwordIn");
 
 passsword.addEventListener("input", function (event) {
-  if (passsword.validity.patternMismatch) {
-    passsword.setCustomValidity("You need to add at least one of these characters: ?!=#+-_%§");
-  } else {
-    passsword.setCustomValidity("");
-  }
+    if (passsword.validity.patternMismatch) {
+        passsword.setCustomValidity("You need to add at least one of these characters: ?!=#+-_%§");
+    } else {
+        passsword.setCustomValidity("");
+    }
 });
 
